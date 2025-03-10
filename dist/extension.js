f2da2a3521d5a1d10189b32d40457d27943afaba
tmthyDXTR <langmi89@gmail.com>
2025-03-10 09:46:22 +0100

    nodemodules gitignore

--- dist/extension.js
+++ dist/extension.js
@@ -1,1 +1,128 @@
-(()=>{"use strict";var e={265:function(e,t,r){var n,i=this&&this.__createBinding||(Object.create?function(e,t,r,n){void 0===n&&(n=r);var i=Object.getOwnPropertyDescriptor(t,r);i&&!("get"in i?!t.__esModule:i.writable||i.configurable)||(i={enumerable:!0,get:function(){return t[r]}}),Object.defineProperty(e,n,i)}:function(e,t,r,n){void 0===n&&(n=r),e[n]=t[r]}),o=this&&this.__setModuleDefault||(Object.create?function(e,t){Object.defineProperty(e,"default",{enumerable:!0,value:t})}:function(e,t){e.default=t}),c=this&&this.__importStar||(n=function(e){return n=Object.getOwnPropertyNames||function(e){var t=[];for(var r in e)Object.prototype.hasOwnProperty.call(e,r)&&(t[t.length]=r);return t},n(e)},function(e){if(e&&e.__esModule)return e;var t={};if(null!=e)for(var r=n(e),c=0;c<r.length;c++)"default"!==r[c]&&i(t,e,r[c]);return o(t,e),t});Object.defineProperty(t,"__esModule",{value:!0}),t.activate=function(e){let t=a.commands.registerCommand("swapCharTypo",(()=>{const e=a.window.activeTextEditor;if(e){const t=e.document,r=e.selection.active,n=t.lineAt(r.line).text;if(r.character>=2){const t=n.substring(0,r.character-2)+n.charAt(r.character-1)+n.charAt(r.character-2)+n.substring(r.character);e.edit((e=>{e.replace(new a.Range(new a.Position(r.line,0),new a.Position(r.line,n.length)),t)}))}}}));e.subscriptions.push(t)},t.deactivate=function(){};const a=c(r(398))},398:e=>{e.exports=require("vscode")}},t={},r=function r(n){var i=t[n];if(void 0!==i)return i.exports;var o=t[n]={exports:{}};return e[n].call(o.exports,o,o.exports,r),o.exports}(265);module.exports=r})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.activate = activate;
+exports.deactivate = deactivate;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = __importStar(__webpack_require__(1));
+function activate(context) {
+    let disposable = vscode.commands.registerCommand('swapCharTypo', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            const position = editor.selection.active;
+            const line = document.lineAt(position.line).text;
+            // Find the last two non-whitespace characters
+            let idx1 = -1, idx2 = -1;
+            for (let i = line.length - 1; i >= 0; i--) {
+                if (!/\s/.test(line[i])) {
+                    if (idx1 === -1) {
+                        idx1 = i;
+                    }
+                    else {
+                        idx2 = i;
+                        break;
+                    }
+                }
+            }
+            // If there are fewer than two non-whitespace characters, do nothing
+            if (idx1 === -1 || idx2 === -1)
+                return;
+            const newText = line.substring(0, idx2) +
+                line.charAt(idx1) +
+                line.substring(idx2 + 1, idx1) +
+                line.charAt(idx2) +
+                line.substring(idx1 + 1);
+            editor.edit(editBuilder => {
+                editBuilder.replace(new vscode.Range(new vscode.Position(position.line, 0), new vscode.Position(position.line, line.length)), newText);
+            });
+        }
+    });
+    context.subscriptions.push(disposable);
+}
+function deactivate() { }
+
+
+/***/ }),
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("vscode");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__(0);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -10,19 +10,34 @@ export function activate(context: vscode.ExtensionContext) {
             const position = editor.selection.active;
             const line = document.lineAt(position.line).text;
 
-            if (position.character >= 2) { 
-                const newText = line.substring(0, position.character - 2) +
-                                line.charAt(position.character - 1) +
-                                line.charAt(position.character - 2) +
-                                line.substring(position.character);
-
-                editor.edit(editBuilder => {
-                    editBuilder.replace(new vscode.Range(
-                        new vscode.Position(position.line, 0),
-                        new vscode.Position(position.line, line.length)
-                    ), newText);
-                });
+            // Find the last two non-whitespace characters
+            let idx1 = -1, idx2 = -1;
+            for (let i = line.length - 1; i >= 0; i--) {
+                if (!/\s/.test(line[i])) {
+                    if (idx1 === -1) {
+                        idx1 = i;
+                    } else {
+                        idx2 = i;
+                        break;
+                    }
+                }
             }
+
+            // If there are fewer than two non-whitespace characters, do nothing
+            if (idx1 === -1 || idx2 === -1) return;
+
+            const newText = line.substring(0, idx2) +
+                            line.charAt(idx1) +
+                            line.substring(idx2 + 1, idx1) +
+                            line.charAt(idx2) +
+                            line.substring(idx1 + 1);
+
+            editor.edit(editBuilder => {
+                editBuilder.replace(new vscode.Range(
+                    new vscode.Position(position.line, 0),
+                    new vscode.Position(position.line, line.length)
+                ), newText);
+            });
         }
     });
 
